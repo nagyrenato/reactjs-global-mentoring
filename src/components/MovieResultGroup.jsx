fe84023ba4e8844caee99acab00c3f3b31f32c68
Balazs Havasi <balazs_havasi@epam.com>
2020-06-22 22:28:14 +0200

    Redesign movies list page

--- src/components/MovieResultGroup.jsx
+++ src/components/MovieResultGroup.jsx
@@ -1,105 +1,17 @@
 import React from "react";
-import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import Moment from "react-moment";
-import Container from "react-bootstrap/Container";
+import MovieListItem from "./MovieListItem";
 
 function MovieResultGroup({ movies }) {
-  const movieItems = movies.map((movie) => (
-    <>
-      <div
-        key={movie.id}
-        className="text-white"
-      >
-        <Row>
-          <Col>
-            <Button
-              className={"btn btn-circle-v2 btn-circle-rounded btn-secondary"}
-            >
-              <i className="fa fa-ellipsis-v"></i>
-            </Button>
-            <Image
-              fluid
-              src={movie.poster_path}
-              onError={(e) => {
-                e.target.onerror = null;
-                e.target.src =
-                  "https://via.placeholder.com/500x750.png&text=No+Picture";
-              }}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col className={"col-sm-9"}>
-            <h5>{movie.title}</h5>
-          </Col>
-          <Col className={"col-sm-3"}>
-            <p className={"border-info"}>
-              <Moment date={movie.release_date} format={"YYYY"}></Moment>
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {" "}
-            {movie.genres
-              .map((genre) => {
-                return `${genre}`;
-              })
-              .reduce((prev, curr) => [prev, ", ", curr])}
-          </Col>
-        </Row>
-      </div>
-    </>
-  ));
-
-  function createRows() {
-    let rows = [];
-    for (let i = 0; i < movieItems.length; i++) {
-      rows.push(
-        <Row key={"row" + i}>
-          <Col>{movieItems[i]}</Col>
-          <Col>{movieItems[i + 1]}</Col>
-          <Col>{movieItems[i + 2]}</Col>
-        </Row>
-      );
-      i = i + 3;
-    }
-    return rows;
-  }
-
-  function EmptyMovieList() {
-    return (
-      <Container
-        className={
-          "bg-dark text-capitalize EmptyMovieList-height d-flex align-items-center justify-content-center"
-        }
-      >
-        <h1 className={"bg-dark text-white"}>No movie found</h1>
-      </Container>
-    );
-  }
-
-  function MovieList() {
-    return (
-        <>
-          {createRows().map((movieRow) => {
-            return movieRow;
-          })}
-        </>
-    );
-  }
-
   return (
-    <>
-      {movieItems.length === 0 ? (
-        <EmptyMovieList></EmptyMovieList>
-      ) : (
-        <MovieList></MovieList>
-      )}
-    </>
+      <Row>
+        {movies.map((movie, key) => (
+            <Col lg={4} md={6} sm={12}>
+              <MovieListItem key={key} movie={movie}/>
+            </Col>
+        ))}
+      </Row>
   );
 }
 
