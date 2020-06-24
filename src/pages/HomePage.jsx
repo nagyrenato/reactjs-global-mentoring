f146e9751394ad85511c18b770fef1c577197df5
Renato Nagy <v-renagy@expedia.com>
2020-06-24 08:16:27 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import SearchBar from "../components/SearchBar";
 import GenreGroup from "../components/GenreGroup";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Footer from "../components/Footer";
 import SortBy from "../components/SortBy";
 import EmptyMovieResultGroup from "../components/EmptyMovieResultGroup";
@@ -51,18 +52,22 @@ class HomePage extends React.Component {
     return (
       <Container>
         <Row>
-          <SearchBar/>
+          <SearchBar />
         </Row>
         <Row className={"movie-list-background d-grid px-5 py-3"}>
           <Row className={"filter-bar"}>
             <Col className={"col-sm-9"}>
-              <GenreGroup changeGenre={this.changeGenre}/>
+              <GenreGroup changeGenre={this.changeGenre} />
             </Col>
-            <Col className={"col-sm-3 justify-content-end align-items-center d-flex"}>
+            <Col
+              className={
+                "col-sm-3 justify-content-end align-items-center d-flex"
+              }
+            >
               <SortBy
-                  defaultSortBy={this.state.sortBy}
-                  changeSortBy={this.changeSortBy}
-                  className={"align-content-end"}
+                defaultSortBy={this.state.sortBy}
+                changeSortBy={this.changeSortBy}
+                className={"align-content-end"}
               />
             </Col>
           </Row>
