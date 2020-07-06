c02ad4c484ad2221528a59f08da1697fba754766
Balazs Havasi <balazs_havasi@epam.com>
2020-07-06 10:22:45 +0200

    Rename files starting with uppercase letter

---
+++ src/store/reducers/Movies.js
@@ -0,0 +1,28 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const moviesReducer = (movies = [], action) => {
+  switch (action.type) {
+    case actionTypes.SET_MOVIES:
+      return [...action.payload];
+
+    case actionTypes.ADD_MOVIE:
+      return [...movies].concat(action.payload);
+
+    case actionTypes.UPDATE_MOVIE: {
+      const updatedMovies = [...movies];
+      const indexOfUpdatedMovie = updatedMovies.findIndex(
+        (movie) => movie.id === action.payload.id
+      );
+      updatedMovies[indexOfUpdatedMovie] = action.payload;
+      return updatedMovies;
+    }
+
+    case actionTypes.DELETE_MOVIE:
+      return [...movies].filter((movie) => movie.id !== action.payload.id);
+
+    default:
+      return movies;
+  }
+};
+
+export default moviesReducer;
---
+++ src/utils/SortByOptions.js
@@ -0,0 +1,3 @@
+export const RELEASE_DATE = "release date";
+export const RATING = "rating";
+export const TITLE = "title";
