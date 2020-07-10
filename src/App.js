de29a8326144d46331bcfa0fce166dab33fccb17
Renato Nagy <v-renagy@expedia.com>
2020-07-10 16:39:13 +0200

    Fixed redirecting.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/style.css";
 import ErrorBoundary from "./components/ErrorBoundary";
@@ -11,10 +16,12 @@ function App() {
   return (
     <ErrorBoundary>
       <Router>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/movies/:movieId" component={MovieDetailPage} />
-        <Route path="/movie-not-found" component={NotFoundPage} />
-        <Redirect to="/movie-not-found" />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/movies/:movieId" component={MovieDetailPage} />
+          <Route path="/not-found" component={NotFoundPage} />
+          <Redirect to="/not-found" />
+        </Switch>
       </Router>
     </ErrorBoundary>
   );
